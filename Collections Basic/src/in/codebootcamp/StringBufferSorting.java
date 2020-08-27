52748df8223eb2948e0aa6b22bd9650fe1484b92
sumit241193 <sumit.agrawal93@gmail.com>
2020-08-27 20:53:54 +0530

    Sorted StringBuffer According to Length

--- Collections Basic/src/in/codebootcamp/StringBufferSorting.java
+++ Collections Basic/src/in/codebootcamp/StringBufferSorting.java
@@ -10,6 +10,14 @@ class MyComparatorString implements Comparator {
 		String s1=o1.toString();
 		String s2=o2.toString();
 		
+		int l1=s1.length();
+		int l2=s2.length();
+		
+		if(l1<l2)
+			return -1;
+		else if(l1>l2)
+			return +1;
+		else
 		return s1.compareTo(s2);
 		
 	} 
@@ -24,7 +32,7 @@ public class StringBufferSorting {
 		
 		t.add(new StringBuffer("Shaktiman"));
 		t.add(new StringBuffer("Jackal"));
-		t.add(new StringBuffer("Ravan"));
+		t.add(new StringBuffer("sanju"));
 		t.add(new StringBuffer("Ironman"));
 		t.add(new StringBuffer("Sanju"));
 		
